6af9e4c12fe478b24d120b7a96c46b528e2d257f
Wagner Santos <wagnerstss@gmail.com>
2023-03-09 14:29:40 -0300

    Daily Billing

---
+++ Pergunta3/Entities/Faturamento.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Pergunta3.Entities
+{
+    public class Faturamento
+    {
+        public int Dia { get; set; }
+        public double Valor { get; set; }
+
+        public double menorValor (List<Faturamento> fat)
+        {
+          var menor = fat.Select(x => x.Valor).Where(x => x > 0).ToList().Min();
+            return menor;
+        }
+
+        public double maiorValor (List<Faturamento> fat)
+        {
+            var maior = fat.Select(x => x.Valor).ToList().Max();
+            return maior;
+        }
+
+        public double mediaMensal (List<Faturamento> fat)
+        {
+            int contador = 0;
+            double sumTotal = 0;
+            foreach ( Faturamento list in fat)
+            {
+                if (list.Valor > 0)
+                {
+                    contador++;
+                }
+                sumTotal = sumTotal + list.Valor;
+            }
+            var media = sumTotal / contador;
+            return media;
+        }
+
+        public int numeroDeDias (double media, List<Faturamento> fat)
+        {
+            int contador = 0;
+                
+                foreach (Faturamento list in fat)
+                {
+                    if (list.Valor > media)
+                    {
+                       contador++;
+                    }
+                }
+            return contador;
+        }
+    }
+}
---
+++ Pergunta3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Newtonsoft.Json;
+
+using Pergunta3.Entities;
+
+namespace Pergunta3
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\FaturamentoMensal.json");
+            var factor = JsonConvert.DeserializeObject<List<Faturamento>>(json);
+
+            Faturamento faturamento = new Faturamento();
+            var menor = faturamento.menorValor(factor);
+            
+            Console.WriteLine("Menor valor de faturamento ocorrido em um dia do mês: " + menor);
+
+            var maior = faturamento.maiorValor(factor);
+            Console.WriteLine("Maior valor de faturamento ocorrido em um dia do mês: " + maior);
+
+            var media = faturamento.mediaMensal(factor);
+            
+
+            var dias = faturamento.numeroDeDias(media, factor);
+            Console.WriteLine(" Número de dias no mês em que o valor de faturamento diário foi superior à média mensal: " + dias);
+        }
+    }
+}
